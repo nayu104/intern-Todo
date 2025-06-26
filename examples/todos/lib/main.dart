9f71c08d065a3b6860e2caa78c15b330c21ef4eb
nayu <ose794@gamil.com>
2025-06-27 01:38:35 +0900

    initial commit

--- examples/todos/lib/main.dart
+++ examples/todos/lib/main.dart
@@ -11,41 +11,20 @@ final activeFilterKey = UniqueKey();
 final completedFilterKey = UniqueKey();
 final allFilterKey = UniqueKey();
 
-/// Creates a [TodoList] and initialize it with pre-defined values.
-///
-/// We are using [StateNotifierProvider] here as a `List<Todo>` is a complex
-/// object, with advanced business logic like how to edit a todo.
 final todoListProvider = NotifierProvider<TodoList, List<Todo>>(TodoList.new);
 
-/// The different ways to filter the list of todos
 enum TodoListFilter {
   all,
   active,
   completed,
 }
 
-/// The currently active filter.
-///
-/// We use [StateProvider] here as there is no fancy logic behind manipulating
-/// the value since it's just enum.
 final todoListFilter = StateProvider((_) => TodoListFilter.all);
 
-/// The number of uncompleted todos
-///
-/// By using [Provider], this value is cached, making it performant.\
-/// Even multiple widgets try to read the number of uncompleted todos,
-/// the value will be computed only once (until the todo-list changes).
-///
-/// This will also optimize unneeded rebuilds if the todo-list changes, but the
-/// number of uncompleted todos doesn't (such as when editing a todo).
 final uncompletedTodosCount = Provider<int>((ref) {
   return ref.watch(todoListProvider).where((todo) => !todo.completed).length;
 });
 
-/// The list of todos after applying of [todoListFilter].
-///
-/// This too uses [Provider], to avoid recomputing the filtered list unless either
-/// the filter of or the todo-list updates.
 final filteredTodos = Provider<List<Todo>>((ref) {
   final filter = ref.watch(todoListFilter);
   final todos = ref.watch(todoListProvider);
@@ -217,13 +196,6 @@ class Title extends StatelessWidget {
   }
 }
 
-/// A provider which exposes the [Todo] displayed by a [TodoItem].
-///
-/// By retrieving the [Todo] through a provider instead of through its
-/// constructor, this allows [TodoItem] to be instantiated using the `const` keyword.
-///
-/// This ensures that when we add/remove/edit todos, only what the
-/// impacted widgets rebuilds, instead of the entire list of items.
 final _currentTodo = Provider<Todo>(
   dependencies: const [],
   (ref) => throw UnimplementedError(),
