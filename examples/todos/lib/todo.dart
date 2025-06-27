41b2900c5c6ccd010f82d6470275471974256901
nayu <ose794@gamil.com>
2025-06-27 16:27:51 +0900

    fix: use freezed/riverpod 2系で依存競合解消＆正常動作確認

--- examples/todos/lib/todo.dart
+++ examples/todos/lib/todo.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:riverpod/riverpod.dart';
 import 'package:uuid/uuid.dart';
-
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'todo.freezed.dart';
